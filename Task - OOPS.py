98c731251d007d37352a715bf2de9fe8a8d39be2
SuryaPain <160389278+SuryaPain@users.noreply.github.com>
2024-05-06 12:06:15 +0530

    Add files via upload

---
+++ Task - OOPS.py
@@ -0,0 +1,6 @@
+numbers = [10, 501, 22, 37, 100, 999, 87, 351]
+class Circle:
+    def __init__(self, numbers):
+        self.numbers = numbers
+circle = Circle(numbers)
+print("Circle numbers:", circle.numbers)
